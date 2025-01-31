59273801d635d22e6eec90d2ed7b83b650f30f9d
fred077a <fred18q3@stud.kea.dk>
2025-01-31 17:43:13 +0100

    test file created

---
+++ HotelBooking.Core/HotelBooking.Core.Tests/HotelBookingTests.cs
@@ -0,0 +1,14 @@
+using Moq;
+
+namespace HotelBooking.Core.Tests
+{
+    public class HotelBookingTests
+    {
+        private readonly Mock<IRepository<Booking>> _mockBookingRepo;
+        private readonly Mock<IRepository<Room>> _mockRoomRepo;
+        private readonly BookingManager _bookingManager;
+        
+        
+        
+    }
+}
